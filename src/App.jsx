4bcb1aa1c107309f7a6f42df288bb6e2431180d6
Kabina Thapa <thapakabina28@gmail.com>
2023-06-19 18:25:07 -0400

    React-vite env setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  const[data, setData]=useState('')
+  const fetchData=()=>{
+    axios.get('')
+  }
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
