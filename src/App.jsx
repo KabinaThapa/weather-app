74a439b94db42da914db969ccfa0c62a4255e68d
Kabina Thapa <thapakabina28@gmail.com>
2023-06-24 16:25:03 -0400

    Install Axios library

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  //useState to update data
   const[data, setData]=useState('')
+  //api key
   const API_Key= '435c048a9c6bca0718dee1b66c720006'
+  const url ='https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+  
   const fetchData=()=>{
     axios.get('')
   }
