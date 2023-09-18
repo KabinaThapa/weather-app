2e4a3142a499c7ab45aff9f9e77479701a3bc42f
Kabina Thapa <thapakabina28@gmail.com>
2023-09-18 18:41:41 -0400

    feat(animation):implement rain animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import React from "react";
 import axios from "axios";
 import DailyWeather from "./Components/dailyWeather/dailyWeather";
+import rainy from './picture/rainy.svg'
 
 import "./App.css";
 
@@ -18,6 +19,26 @@ function App() {
 <div className="flex mt-8"> 
   <CurrentWeather/>
   <DailyWeather/>
+  <div className="border  w-96 h-96 relative overflow-hidden ">
+    <div className="raindrops left-8 top-3 m-2"></div>
+    <div className="raindrop left- top-16  m-8"></div>
+    <div className="raindrops left-6 top-8 m-2"></div>
+    <div className="raindrop left-1 top-12  m-2"></div>
+    <div className="raindrops left-32 top-14  m-2"></div>
+    <div className="raindrop left-9 top-15 m-2"></div>
+    <div className="raindrops left-10 top-16  m-2"></div>
+    <div className="raindrop left-32 top-20  m-2"></div>
+    <div className="raindrops left-1 top-1 m-2"></div>
+    <div className="raindrop left-4 top-2  m-8"></div>
+    <div className="raindrops left-24 top-8 m-2"></div>
+    <div className="raindrop left-32 top-0  m-2"></div>
+    <div className="raindrops left-20 top-14  m-2"></div>
+    <div className="raindrop left-12 top-15 m-2"></div>
+    <div className="raindrops left-6 top-28  m-2"></div>
+    <div className="raindrop left-25 top-20  m-2"></div>
+   
+    <img src={rainy} className="border object-cover w-full h-full"/></div>
+  
 </div>
 
     </>
